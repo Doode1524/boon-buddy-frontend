25d8c8c95182f40f2d02bb25cc96ca080b8fbab0
joey <doode1524@gmail.com>
2021-08-24 16:33:05 -0400

    fetchGods

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [gods, setGods] = useState([])
+
+  const fetchGods = () => {
+    fetch("http://localhost:3000/gods")
+    .then(res => res.json())
+    .then(data => setGods(data))
+  }
+  
+  useEffect(() => {
+    fetchGods()
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello Zagreus</h1>
